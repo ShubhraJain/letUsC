0d2ec6c7a8ad5b57996fc8e23716b92fb7f75e10
ShubhraJain <00sankalp@gmail.com>
2014-03-09 22:19:38 -0700

    floating point comparison

--- pointsOnALine.c
+++ pointsOnALine.c
@@ -7,16 +7,19 @@ OUTPUT:
 Print “Yes” if all the points fall on straight line, “No” otherwise.
 */
 
-# include <stdio.h>
+#include <stdio.h>
+#include <math.h>
+
 int main() {
    int x1, y1; 
    int x2, y2;
    int x3, y3;
    scanf("%d%d%d%d%d%d", &x1, &y1, &x2, &y2, &x3, &y3);
+   
    if ((x2-x1)==0 && (x3-x2)==0) {
       printf("Yes");
-   }else {
-      if((y2-y1)/(x2-x1)==(y3-y2)/(x3-x2)) {
+   } else {
+      if (fabs((y2-y1+0.0)/(x2-x1) - (y3-y2+0.0)/(x3-x2)) < 0.00001) {
           printf("Yes");
       } else printf("No");
    }
